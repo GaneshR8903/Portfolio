6110c76fe02fa16dfdcbac55d28df5706c127aa5
GaneshR8903 <ganesh.nexitance@gmail.com>
2024-12-18 21:22:54 +0530

    my portfolio Website Pre completion

--- js/script.js
+++ js/script.js
@@ -23,11 +23,20 @@ window.onscroll=() =>{
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
+
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
         }
     });
 
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky',window.scrollY > 100);
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
